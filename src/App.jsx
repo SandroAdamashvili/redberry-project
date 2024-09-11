97d657960420ec4412e3ad95ca9a64a51d53b50f
Sandro <san.adamashvili@gmail.com>
2024-09-11 18:36:11 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Logo from "./assets/rdbr-logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <img src={Logo} alt="logo" />
+    </>
+  );
+}
+
+export default App;
